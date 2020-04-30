523464e7fa9c72495b16ed26be72659dbab7d942
Shubha Patil <53892562+shubhapatil154@users.noreply.github.com>
2020-04-30 13:39:05 +0530

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,126 +1,106 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-#define N 3 
+class Node:
+    def __init__(self,data,level,fval):
+        """ Initialize the node with the data, level of the node and the calculated fvalue """
+        self.data = data
+        self.level = level
+        self.fval = fval
+    def generate_child(self):
+        """ Generate child nodes from the given node by moving the blank space
+            either in the four directions {up,down,left,right} """
+        x,y = self.find(self.data,'_')
+        """ val_list contains position values for moving the blank space in either of
+            the 4 directions [up,down,left,right] respectively. """
+        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
+        children = []
+        for i in val_list:
+            child = self.shuffle(self.data,x,y,i[0],i[1])
+            if child is not None:
+                child_node = Node(child,self.level+1,0)
+                children.append(child_node)
+        return children
 
-struct Node 
-{ 
-	Node* parent;  
-	int mat[N][N]; 
-	int x, y; 
-	int cost; 
-	int level; 
-}; 
-int printMatrix(int mat[N][N]) 
-{ 
-	for (int i = 0; i < N; i++) 
-	{ 
-		for (int j = 0; j < N; j++) 
-			printf("%d ", mat[i][j]); 
-		printf("\n"); 
-	} 
-} 
-Node* newNode(int mat[N][N], int x, int y, int newX, 
-			int newY, int level, Node* parent) 
-{ 
-	Node* node = new Node; 
-	node->parent = parent; 
-	memcpy(node->mat, mat, sizeof node->mat); 
-	swap(node->mat[x][y], node->mat[newX][newY]); 
-	node->cost = INT_MAX; 
-	node->level = level;  
-	node->x = newX; 
-	node->y = newY;
-	return node; 
-} 
+    def shuffle(self,puz,x1,y1,x2,y2):
+        """ Move the blank space in the given direction and if the position value are out
+            of limits the return None """
+        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
+            temp_puz = []
+            temp_puz = self.copy(puz)
+            temp = temp_puz[x2][y2]
+            temp_puz[x2][y2] = temp_puz[x1][y1]
+            temp_puz[x1][y1] = temp
+            return temp_puz
+        else:
+            return None
+    def copy(self,root):
+        """ Copy function to create a similar matrix of the given node"""
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp
 
-int row[] = { 1, 0, -1, 0 }; 
-int col[] = { 0, -1, 0, 1 };
-
-int calculateCost(int initial[N][N], int final[N][N]) 
-{ 
-	int count = 0; 
-	for (int i = 0; i < N; i++) 
-	for (int j = 0; j < N; j++) 
-		if (initial[i][j] && initial[i][j] != final[i][j]) 
-		count++; 
-	return count; 
-} 
-
-int isSafe(int x, int y) 
-{ 
-	return (x >= 0 && x < N && y >= 0 && y < N); 
-} 
-
-void printPath(Node* root) 
-{ 
-	if (root == NULL) 
-		return; 
-	printPath(root->parent); 
-	printMatrix(root->mat); 
-
-	printf("\n"); 
-} 
-
-struct comp 
-{ 
-	bool operator()(const Node* lhs, const Node* rhs) const
-	{ 
-		return (lhs->cost + lhs->level) > (rhs->cost + rhs->level); 
-	} 
-}; 
-
-void solve(int initial[N][N], int x, int y, 
-		int final[N][N]) 
-{ 
-	
-	priority_queue<Node*, std::vector<Node*>, comp> pq; 
-	Node* root = newNode(initial, x, y, x, y, 0, NULL); 
-	root->cost = calculateCost(initial, final); 
-	pq.push(root); 
-
-	while (!pq.empty()) 
-	{ 
-		Node* min = pq.top(); 
-		pq.pop(); 
-		if (min->cost == 0) 
-		{ 
-			printPath(min); 
-			return; 
-		} 
-		for (int i = 0; i < 4; i++) 
-		{ 
-			if (isSafe(min->x + row[i], min->y + col[i])) 
-			{ 
-				Node* child = newNode(min->mat, min->x, 
-							min->y, min->x + row[i], 
-							min->y + col[i], 
-							min->level + 1, min); 
-				child->cost = calculateCost(child->mat, final); 
-				pq.push(child); 
-			} 
-		} 
-	} 
-} 
-
-int main() 
-{ 
-	int initial[N][N] = 
-	{ 
-		{1, 2, 3}, 
-		{5, 6, 0}, 
-		{7, 8, 4} 
-	}; 
-
-	int final[N][N] = 
-	{ 
-		{1, 2, 3}, 
-		{4, 5, 6}, 
-		{7, 8, 0} 
-	}; 
-	int x = 1, y = 2; 
-
-	solve(initial, x, y, final); 
-
-	return 0; 
-}
-
+    def find(self,puz,x):
+        """ Specifically used to find the position of the blank space """
+        for i in range(0,len(self.data)):
+            for j in range(0,len(self.data)):
+                if puz[i][j] == x:
+                    return i,j
+class Puzzle:
+    def __init__(self,size):
+        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
+        self.n = size
+        self.open = []
+        self.closed = []
+    def accept(self):
+        """ Accepts the puzzle from the user """
+        puz = []
+        for i in range(0,self.n):
+            temp = input().split(" ")
+            puz.append(temp)
+        return puz
+    def f(self,start,goal):
+        """ Heuristic Function to calculate hueristic value f(x) = h(x) + g(x) """
+        return self.h(start.data,goal)+start.level
+    def h(self,start,goal):
+        """ Calculates the different between the given puzzles """
+        temp = 0
+        for i in range(0,self.n):
+            for j in range(0,self.n):
+                if start[i][j] != goal[i][j] and start[i][j] != '_':
+                    temp += 1
+        return temp
+    def process(self):
+        """ Accept Start and Goal Puzzle state"""
+        print("Enter the start state matrix \n")
+        start = self.accept()
+        print("Enter the goal state matrix \n")
+        goal = self.accept()
+        start = Node(start,0,0)
+        start.fval = self.f(start,goal)
+        """ Put the start node in the open list"""
+        self.open.append(start)
+        print("\n\n")
+        while True:
+            cur = self.open[0]
+            print("")
+            print("  | ")
+            print("  | ")
+            print(" \\\'/ \n")
+            for i in cur.data:
+                for j in i:
+                    print(j,end=" ")
+                print("")
+            """ If the difference between current and goal node is 0 we have reached the goal node"""
+            if(self.h(cur.data,goal) == 0):
+                break
+            for i in cur.generate_child():
+                i.fval = self.f(i,goal)
+                self.open.append(i)
+            self.closed.append(cur)
+            del self.open[0]
+            """ sort the opne list based on f value """
+            self.open.sort(key = lambda x:x.fval,reverse=False)
+puz = Puzzle(8)
+puz.process()
